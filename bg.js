739aed31af9ff497a1a5e578b0e3c2c06a8ab549
chestozo <chestozo@yandex-team.ru>
2012-04-15 13:55:43 +0400

    Initial commit.
    Extension does the same, as bookmarklet.

---
+++ bg.js
@@ -0,0 +1,13 @@
+;(function(){
+    $(initButton);
+    function initButton() {
+        chrome.browserAction.onClicked.addListener(function(tab) {
+            chrome.tabs.executeScript(
+                tab.id,
+                {
+                    "code":"function iprl5(){var d=document,z=d.createElement('scr'+'ipt'),b=d.body,l=d.location;try{if(!b)throw(0);d.title='(Saving...) '+d.title;z.setAttribute('src',l.protocol+'//www.instapaper.com/j/83fI1LqqAEzt?u='+encodeURIComponent(l.href)+'&t='+(new Date().getTime()));b.appendChild(z);}catch(e){alert('Please wait until the page has loaded.');}}iprl5();"
+                }
+            );
+        });
+    }
+}());
