983fec951fcc84c04eb35e8c700e0d40f15f5ad1
chestozo <chestozo@gmail.com>
2015-11-08 21:53:02 +0300

    context menu function added for outlining elements on the page

--- bg.js
+++ bg.js
@@ -1,3 +1,23 @@
+// Create context menu item for posting.
+chrome.contextMenus.create({
+    title: 'Dev: Outline elements',
+    type: 'normal',
+    contexts: [ 'all' ],
+    onclick: function(info, tab) {
+        var data = {
+            caption: tab.title,
+            link: tab.url
+        };
+
+        chrome.tabs.executeScript(
+            tab.id,
+            {
+                code: "([]).forEach.call(document.querySelectorAll('*'), function(el) { el.style['box-shadow'] = 'inset 0 0 1px red'; })"
+            }
+        );
+    }
+});
+
 ;(function(){
     $(initButton);
     function initButton() {
